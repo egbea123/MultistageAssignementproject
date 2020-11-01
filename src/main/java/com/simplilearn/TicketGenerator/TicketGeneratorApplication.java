a0b55f9e87433b2b7d78f3d41532038c80fc55ce
egbea123 <57382885+egbea123@users.noreply.github.com>
2020-10-31 21:09:38 -0400

    Update TicketGeneratorApplication.java

--- src/main/java/com/simplilearn/TicketGenerator/TicketGeneratorApplication.java
+++ src/main/java/com/simplilearn/TicketGenerator/TicketGeneratorApplication.java
@@ -11,9 +11,8 @@ public class TicketGeneratorApplication {
          double max=9999;
                  double x = (int)(Math.random()*((max-min)+1))+min;
                  return x;
-
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TicketGeneratorApplication.class, args);
 	}
-    }
 }
